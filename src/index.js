2764da412e24a80e3a7953667f38b1d91fbf1609
eflaatten <elliotflaatten@live.com>
2024-06-27 13:49:41 -0500

    index.html

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ const startGameBtn = document.getElementById('start-game')
 const startGame = () => {
     console.log('Game starting...')
 
-    window.location.href = 'game.html';
+    window.location.href = '../pages/game.html';
 }
