7a2f641098d4c796a51da414266065683f594b2f
eflaatten <elliotflaatten@live.com>
2024-07-20 23:05:15 -0500

    hotfix path names

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const brickBreaker = () => {
     console.log('Launching Brick Breaker...')
 
-    window.location.href = '/brickBreaker/pages/index.html'
+    window.location.href = './brickBreaker/pages/index.html'
 }
 
 const towersOfHanoi = () => {
     console.log('Launching Towers of Hanoi...')
 
-    window.location.href = '/towersOfHanoi/pages/index.html'
+    window.location.href = './towersOfHanoi/pages/index.html'
 }
 
 const ticTacToe = () => {
     console.log('Launching Tic Tac Toe...')
 
-    window.location.href = '/TTT/pages/index.html'
+    window.location.href = '.arcadia/TTT/pages/index.html'
 }
 
 const hangman = () => {
     console.log('Launching Hangman...')
 
-    window.location.href = '/hangman/pages/index.html'
+    window.location.href = './hangman/pages/index.html'
 }
